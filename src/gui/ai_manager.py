96f1dec7f1b6e6f2236ad265451c6d183a8d4be0
24127250-PhanQuangTien <tienphan4399@gmail.com>
2026-04-22 10:47:37 +0700

    Output_BC+BTR+A*

--- src/gui/ai_manager.py
+++ src/gui/ai_manager.py
@@ -168,7 +168,7 @@ class AISolverManager:
                 result = solver.run(case, step_callback=on_step, input_file=input_file, output_file = output_file)
             elif solver_name == "forward":
                 solver = ForwardBacktrackSolver()
-                result = solver.run(case, step_callback=on_step, input_file=input_file, output_file = output_file)
+                result = solver.run(case, step_callback=on_step, input_file=input_file)
                 solver = ForwardSolver()
                 result = solver.run(case, step_callback=on_step, input_file=input_file, output_file = output_file)
             else:
